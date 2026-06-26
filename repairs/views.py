a35214cb781fa3dbe6264a023f83b80583438400
Yunus Muhammadzoda <muhammadzoda.dev@gmail.com>
2026-06-27 04:03:02 +0500

    add frontend of search

--- repairs/views.py
+++ repairs/views.py
@@ -1,4 +1,86 @@
- query = request.GET.get('q', "").strip()
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import RepairRequest
+from .forms import RepairRequestForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def repair_create_view(request):
+    if request.method == 'POST':
+        form = RepairRequestForm(request.POST)
+        if form.is_valid():
+            repair = form.save(commit=False)
+            repair.customer = request.user
+            repair.save()
+            return redirect('repair_list')
+    else:
+        form = RepairRequestForm()
+    
+    return render(request, 'repairs/repair_form.html', {'form': form})
+
+@login_required
+def repair_list_view(request):
+    repairs = RepairRequest.objects.filter(customer=request.user).order_by("-created_at")
+
+    query = request.GET.get('q', "").strip()
     category = request.GET.get('category', "").strip()
     urgency = request.GET.get('urgency', "").strip()
     status = request.GET.get('status', "").strip()
+
+    if query:
+        repairs = repairs.filter(title__icontains=query)
+
+    if category:
+        repairs = repairs.filter(category=category)
+
+    if urgency:
+        repairs = repairs.filter(urgency=urgency)
+
+    if status:
+        repairs = repairs.filter(status=status)
+
+    context = {
+    "repairs": repairs,
+    "query": query,
+    "selected_category": category,
+    "selected_urgency": urgency,
+    "selected_status": status,
+    "category_choices": RepairRequest.CATEGORY_CHOICES,
+    "urgency_choices": RepairRequest.URGENCY_CHOICES,
+    "status_choices": RepairRequest.STATUS_CHOICES,
+    }
+
+    return render(request, 'repairs/repair_list.html', context=context)
+
+
+@login_required
+def repair_detail_view(request, pk):
+    repair = get_object_or_404(RepairRequest, pk=pk, customer=request.user)
+
+    return render(request, 'repairs/repair_detail.html', {'repair': repair})
+
+@login_required
+def repair_update_view(request, pk):
+    repair = get_object_or_404(RepairRequest, pk=pk, customer=request.user)
+
+    if request.method == 'POST':
+        form = RepairRequestForm(request.POST, instance=repair)
+        if form.is_valid():
+            form.save()
+            return redirect('repair_detail', pk=repair.pk)
+    
+    else:
+        form = RepairRequestForm(instance=repair)
+
+    return render(request, 'repairs/repair_form.html', {'form': form})
+
+
+@login_required
+def repair_delete_view(request, pk):
+    repair = get_object_or_404(RepairRequest, pk=pk, customer=request.user)
+
+    if request.method == 'POST':
+        repair.delete()
+        return redirect('repair_list')
+    
+    else:
+        return render(request, 'repairs/repair_confirm_delete.html', {'repair': repair})
