af5d3fb2fe652a724ba2ab87215a9070e675b901
Yunus Muhammadzoda <muhammadzoda.dev@gmail.com>
2026-06-26 10:15:13 +0500

    Configure custom user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ['username', 'email', 'phone_number', 'user_type', 'is_staff', 'is_active']
+    search_fields = ['username', 'email', 'phone_number']
+    list_filter = ['user_type', 'is_staff', 'is_active']
+
+    fieldsets =  UserAdmin.fieldsets + (
+        (
+            "UstoHub information",
+            {"fields": ('phone_number', 'user_type',)},
+        ),
+    )
+
+
+    add_fieldsets =  UserAdmin.add_fieldsets + (
+        (
+            "UstoHub information",
+            {"fields": ('email', 'phone_number', 'user_type',)},
+        ),
+    )
