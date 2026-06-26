e0f5932aa78f8cb6375c5c04144081c186370463
Yunus Muhammadzoda <muhammadzoda.dev@gmail.com>
2026-06-26 10:15:00 +0500

    Add custom user and email verification models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+
+    USER_TYPE_CHOICES = [
+        ('customer', 'Customer'),
+        ('master', 'Master'),
+    ]
+
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='customer')
+
+    def __str__(self):
+        return self.username
+
+class EmailVerificationCode(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='email_verification')
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.user.username
