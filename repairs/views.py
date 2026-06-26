b9578633d5f174b9bf35fa6688a97191b10996f6
Yunus Muhammadzoda <muhammadzoda.dev@gmail.com>
2026-06-26 22:16:37 +0500

    Change Frontend design and create repair_create function

--- repairs/views.py
+++ repairs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RepairRequestForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def repair_create_view(request):
+    if request.method == 'POST':
+        form = RepairRequestForm(request.POST)
+        if form.is_valid():
+            repair = form.save(commit=False)
+            repair.customer = request.user
+            repair.save()
+            return redirect('register')
+    else:
+        form = RepairRequestForm()
+    
+    return render(request, 'repairs/repair_form.html', {'form': form})
+
