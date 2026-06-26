a1c9f8341de9955d1be4473df9467e71f38f1e3b
Yunus Muhammadzoda <muhammadzoda.dev@gmail.com>
2026-06-26 12:05:03 +0500

    Configure RepairRequest admin

--- repairs/admin.py
+++ repairs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RepairRequest
+
+
+@admin.register(RepairRequest)
+class RepairRequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "customer",
+        "category",
+        "urgency",
+        "status",
+        "created_at",
+    ]
+    list_filter = ["category", "urgency", "status"]
+    search_fields = ["title", "description", "address", "customer__username"]
